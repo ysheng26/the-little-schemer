72b201eb1ac4a9719a57719e28484d42d3f73097
Yun Sheng <uewing@gmail.com>
2025-10-29 08:28:39 -0400

    multiinsertL

--- little-schemer-starter.rkt
+++ little-schemer-starter.rkt
@@ -115,6 +115,7 @@
 ;; subst2
 ;; multirember
 ;; multiinsertR
+;; multiinsertL
 
 
 ;; Write the function lat? using some, but not
@@ -237,8 +238,15 @@
          ((eq? (car xs) old) (cons old (cons new (multiinsertR new old (cdr xs)))))
          (else (cons (car xs) (multiinsertR new old (cdr xs)))))))))
 
-; next multiinsertL
 
+(define multiinsertL
+  (lambda (new old xs)
+    (cond
+      ((null? xs) '())
+      (else
+       (cond
+         ((eq? (car xs) old) (cons new (cons old (multiinsertL new old (cdr xs)))))
+         (else (cons (car xs) (multiinsertL new old (cdr xs)))))))))
 
 
 
